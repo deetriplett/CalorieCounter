7530131bbd596dfcde922109f6a66eb30bd85b85
deetriplett <deetriplett@gmail.com>
2016-09-14 22:06:16 -0400

    Added WriteLine

--- Program.cs
+++ Program.cs
@@ -2,7 +2,8 @@ class Program
 {
     static void Main()
     {
-        //prompt user for calories eaten 
+        //prompt user for calories eaten
+         System.Console.WriteLine("Enter Calories consumed: "); 
         //add calories to total
         //display total number on screen
         //repeat til user quits
