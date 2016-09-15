5bd528ebc5b138eaf4460b0c6fc4f7e6c8c33c32
deetriplett <deetriplett@gmail.com>
2016-09-14 22:16:21 -0400

    Added firstname and entry

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-class Program
-{
-    static void Main()
-    {
+
+        string firstname = "Dawn";
         //prompt user for calories eaten
-         System.Console.WriteLine("Enter Calories consumed: "); 
+        System.Console.WriteLine("Enter Calories consumed: ");
+        
+        string entry = System.Console.ReadLine();
+        
+   
         //add calories to total
         //display total number on screen
         //repeat til user quits
-
-    }
-}
