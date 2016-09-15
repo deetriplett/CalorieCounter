990de1851f02ddfcb07c10f21721554f49f2d88f
deetriplett <deetriplett@gmail.com>
2016-09-14 22:51:10 -0400

    Added System directive
    deleted System from write and read methods

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
+using System; 
 
-        string firstname = "Dawn";
-        //prompt user for calories eaten
-        System.Console.WriteLine("Enter Calories consumed: ");
+namespace calorieCounter { 
+        class Program
+    {
+        static void Main()
+        {
+            string firstname = "Dawn";
+            //prompt user for calories eaten
+            Console.WriteLine("Enter Calories consumed: ");
         
-        string entry = System.Console.ReadLine();
+            string entry = Console.ReadLine();
         
    
-        //add calories to total
-        //display total number on screen
-        //repeat til user quits
+            //add calories to total
+            //display total number on screen
+            //repeat til user quits
+
+        }
+    }
+}
