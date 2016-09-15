07cf04ff236d721f8a6293c326aff355a8ea80e7
deetriplett <deetriplett@gmail.com>
2016-09-14 21:43:28 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+class Program
+{
+    static void Main()
+    {
+        //prompt user for calories eaten 
+        //add calories to total
+        //display total number on screen
+        //repeat til user quits
+
+    }
+}
