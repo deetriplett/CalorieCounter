8bf4764cf0fec88e1a18993527ba62d6ee49c384
deetriplett <deetriplett@gmail.com>
2016-09-14 23:02:40 -0400

    Added writeline to display calorie total

--- Program.cs
+++ Program.cs
@@ -1,19 +1,20 @@
-using System; 
+using System;
 
 namespace calorieCounter { 
         class Program
     {
         static void Main()
         {
-            string firstname = "Dawn";
             //prompt user for calories eaten
             Console.WriteLine("Enter Calories consumed: ");
         
             string entry = Console.ReadLine();
-        
-   
+
+
             //add calories to total
             //display total number on screen
+            Console.WriteLine("You've eaten " + entry + " calories today.");
+
             //repeat til user quits
 
         }
